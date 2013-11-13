269fd0bf26b1e3f8db07b7f4a82e0603196b6999
Jordan Fish <fishjord@msu.edu>
2013-11-13 16:32:15 -0500

    Added a plot script for line plots

---
+++ plotting/plot_two_column_line_plot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+if len(sys.argv) < 2:
+    print >>sys.stderr, "USAGE: plot_two_column_scatter.py <input_file> [title]"
+    sys.exit(1)
+
+x = []
+y = []
+
+xlab = None
+ylab = None
+
+for line in open(sys.argv[1]):
+    if line[0] == "#":
+        lexemes = line[1:].strip().split()
+        xlab = lexemes[0]
+        ylab = lexemes[1]
+    else:
+        lexemes = line.strip().split()
+        if len(lexemes) > 1:
+            x.append(float(lexemes[0]))
+            y.append(float(lexemes[1]))
+
+plt.plot(x, y)
+if len(sys.argv) > 3:
+    plt.title(sys.argv[2])
+if xlab:
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+
+plt.savefig("%s_plot.eps" % sys.argv[1])
